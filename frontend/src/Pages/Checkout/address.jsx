1f2a501d3ebf4c27bfd86322261b10ee38b44d07
kavita mali <bsckavita@gmail.com>
2023-03-29 16:03:57 +0530

    checkout-ui

--- frontend/src/Pages/Checkout/address.jsx
+++ frontend/src/Pages/Checkout/address.jsx
@@ -10,13 +10,28 @@ export const Address = () => {
   <CartNavbar/>
 <div className='cart-container'>
   <div className='cart-products'>
- <h1>address</h1>
+  <div style={{display:"flex" ,justifyContent:"space-around"}}> <h3>Select dilevery Address</h3><h3>+ Add New Address</h3></div>
+<div className='address'>
+  <h2>Name</h2>
+  <p>location</p>
+  <p>state</p>
+  <p>pincode</p>
+  <p>number</p>
+  <button>Edit</button>
+  <button>Dilever to this address</button>
+</div>
 
   </div>
 
   <div className='order-summry'>
-<h1>price</h1>
+<h1>Price Details</h1>
+<div style ={{display:"flex",justifyContent:"space-between"}}><div>Total product Price</div><span>200</span>   </div>
+<hr></hr>
+<div style ={{display:"flex",justifyContent:"space-between",}}><p>Order total</p><p>200</p>   </div>
+<div style={{backgroundColor:"whitesmoke"}}><p>Clicking on ‘Continue’ will not deduct any money</p></div>
+  <button style={{width:"100%",padding:"10px"}}> continue</button>
   </div>
+ 
 </div>
 
     </div>
