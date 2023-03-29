650e651a66f98b67844ac161a2d4f4c2fff27d6e
kavita mali <bsckavita@gmail.com>
2023-03-29 21:06:44 +0530

    folder st updated

--- frontend/src/Components/CartNavbar.jsx
+++ frontend/src/Components/CartNavbar.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import {RxBorderSolid} from "react-icons/rx"
 import {Bs1Circle, Bs2Circle, Bs3Circle, Bs4Circle} from "react-icons/bs"
+import { Box, Divider, Stack } from '@chakra-ui/react'
 export const CartNavbar = () => {
   return (
     <div className='cart-nav'>
         <div>
             <img src ="https://th.bing.com/th/id/OIP.jHb2K9iBd6eLtuKBCiRERgHaB3?w=343&h=88&c=7&r=0&o=5&dpr=1.3&pid=1.7" width="40%"/>
         </div>
-        <div>
-       
-           <Bs1Circle className='nav-icon' size={"25px"}/>
-           <RxBorderSolid className='nav-icon'size={"25px"}/>
-           <Bs2Circle className='nav-icon'size={"25px"}/>
-           <RxBorderSolid className='nav-icon'size={"25px"}/> 
-           <Bs3Circle className='nav-icon'size={"25px"}/>
-           <RxBorderSolid className='nav-icon'size={"25px"}/> 
-           <Bs4Circle className='nav-icon'size={"25px"}/>
-           
-        </div>
+    
+        <Stack direction='row' >
+
+  <Bs1Circle className='nav-icon' size={"100px"}/>
+  <Divider orientation='horizontal' p={6}/>
+  <Bs2Circle className='nav-icon'size={"100px"}/>
+  <Divider orientation='horizontal' p={6}/>
+  <Bs3Circle className='nav-icon'size={"100px"}/>
+   <Divider orientation='horizontal' p={6}/>
+  <Bs4Circle className='nav-icon'size={"100px"}/>
+</Stack>
+          
+
        
 
 
