582bea36bd131735f87a77cc035264cb256afdf6
Pujarini Sahoo <pujarinisahoo95@gmail.com>
2023-03-30 10:25:42 +0530

    day-2 complete

--- frontend/src/Pages/Product/AllProduct.jsx
+++ frontend/src/Pages/Product/AllProduct.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Layout } from '../../Components/Container/Layout'
+import { useEffect } from 'react'
 
 export const AllProduct = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("loaded");
+    }, 3000)
+  }, [])
   return (
     <Layout>
         <div>AllProduct</div>
