cd58c118660cb8e05eb5bad70d22aa11ea6fb7c6
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-03-31 22:01:27 +0530

    aded get data and filtering parameters

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,11 +1,12 @@
 const { ProductModel } = require("../model/product.model");
-
+const { CartModel } = require("../model/cart.model");
+const jwt = require('jsonwebtoken');
 
 // add all products to database
 const addAllProducts = async (req, res) => {
     const payload = req.body;
     try {
-        const allData = await ProductModel.insertMany(payload)
+        await ProductModel.insertMany(payload)
             .then(docs => {
                 console.log("Multiple documents inserted to Product collection");
                 res.status(200).send({ "msg": "Multiple documents inserted to Product collection" })
@@ -22,15 +23,75 @@ const addAllProducts = async (req, res) => {
 
 // get products data
 const getProducts = async (req, res) => {
-    const {price,rating,fabric,sleeveLength,pattern,reviews,category,productFor} = req.params;
-
+    const { price, rating, fabric, sleeveLength, pattern, reviews, category, productFor } = req.query;
+    const filters = {};
+    if (price) {
+        if (price.includes('-')) {
+            const [minPrice, maxPrice] = price.split('-');
+            filters.price = {
+                $gte: Number(minPrice),
+                $lte: Number(maxPrice)
+            };
+        } else {
+            const maxPrice = Number(price);
+            filters.price = {
+                $lte: maxPrice
+            };
+        }
+    }
+    if (rating) {
+        filters.rating = { $gte: Number(rating)};
+    }
+    if (fabric) {
+        filters.fabric = fabric;
+    }
+    if (sleeveLength) {
+        filters.sleeveLength = sleeveLength;
+    }
+    if (pattern) {
+        filters.pattern = pattern;
+    }
+    // if (reviews) {
+    //   filters.reviews = {
+    //     $gte: Number(reviews)
+    //   };
+    // }
+    if (category) {
+        filters.category = category;
+    }
+    if (productFor) {
+        filters.productFor = productFor;
+    }
+    console.log(filters)
     try {
-        
+        const products = await ProductModel.find(filters);
+
+        const token = req.headers.authorization.split(" ")[1];
+        // console.log("token", token)
+        const decoded = jwt.verify(token, "my_signature");
+        // console.log("decoded", decoded)
+
+        const cart = await CartModel.findOne({ "userID": decoded.userID });
+        if (!cart) {
+            res.status(200).send(products);
+        }
+        else {
+            const updatedData = products.map((prod) => {
+                cart.items.map((item) => {
+                    if (prod._id == item.productID) {
+                        prod.addedQuantity = item.quantity;
+                    }
+                })
+            })
+            res.status(200).send(updatedData);
+        }
+
     } catch (error) {
-        
+        res.status(400).send({ "msg": error.message });
     }
 }
 
 
 
-module.exports = { addAllProducts };
+
+module.exports = { addAllProducts, getProducts };
