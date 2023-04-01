7d1e88a0730a219c88c9e0a2a75ccbb91d671e76
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-04-01 20:42:36 +0530

    added sorting and filtering parameters and cart routes.

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -22,82 +22,90 @@ const addAllProducts = async (req, res) => {
 
 // get products data
 const getProducts = async (req, res) => {
-    const { price, rating, fabric, sleeveLength, pattern, reviews, category, productFor } = req.query;
+    const { price, rating, fabric, sleeveLength, pattern, reviews, category, productFor, page = 1, limit = 10, sort } = req.query;
     const filters = {};
-
+  
     if (price) {
-        if (price.includes('-')) {
-            const [minPrice, maxPrice] = price.split('-');
-            filters.price = {
-                $gte: Number(minPrice),
-                $lte: Number(maxPrice)
-            };
-        } else {
-            const maxPrice = Number(price);
-            filters.price = {
-                $lte: maxPrice
-            };
-        }
-    }
-
-    if (rating) {
-        filters.rating = { $gte: Number(rating) };
-    }
-
-    if (fabric) {
-        filters.fabric = fabric;
-    }
-
-    if (sleeveLength) {
-        filters.sleeveLength = sleeveLength;
-    }
-
-    if (pattern) {
-        filters.pattern = pattern;
-    }
-
-    if (category) {
-        filters.category = category;
-    }
-
-    if (productFor) {
-        filters.productFor = productFor;
-    }
-
-    console.log(filters);
-
-    try {
-        const products = await ProductModel.find(filters);
-        const token = req.headers.authorization?.split(" ")[1];
-    
-        if (!token) {
-          // return products if user is not logged in
-          return res.status(200).send(products);
-        }
-    
-        const decoded = jwt.verify(token, "my_signature");
-        const cart = await CartModel.findOne({ "userID": decoded.userID });
-    
-        if (!cart || cart.items.length === 0) {
-          // return products if user is logged in but has no items in cart
-          return res.status(200).send(products);
-        }
-    
-        // update products with added quantity if user is logged in and has items in cart
-        const updatedData = products.map((prod) => {
-          const cartItem = cart.items.find((item) => String(item.productID) === String(prod._id));
-          if (cartItem) {
-            prod.addedQuantity = cartItem.quantity;
-          }
-          return prod;
-        });
-    
-        return res.status(200).send(updatedData);
-    
-      } catch (error) {
-        return res.status(400).send({ "msg": error.message });
+      if (price.includes('-')) {
+        const [minPrice, maxPrice] = price.split('-');
+        filters.price = {
+          $gte: Number(minPrice),
+          $lte: Number(maxPrice)
+        };
+      } else {
+        const maxPrice = Number(price);
+        filters.price = {
+          $lte: maxPrice
+        };
       }
+    }
+  
+    if (rating) {
+      filters.rating = { $gte: Number(rating) };
+    }
+  
+    if (fabric) {
+      filters.fabric = fabric;
+    }
+  
+    if (sleeveLength) {
+      filters.sleeveLength = sleeveLength;
+    }
+  
+    if (pattern) {
+      filters.pattern = pattern;
+    }
+  
+    if (category) {
+      filters.category = category;
+    }
+  
+    if (productFor) {
+      filters.productFor = productFor;
+    }
+  
+    console.log(filters);
+  
+    try {
+      const sortOptions = {};
+      if (sort === 'asc') {
+        sortOptions.price = 1;
+      } else if (sort === 'desc') {
+        sortOptions.price = -1;
+      }
+  
+      const products = await ProductModel.find(filters).sort(sortOptions).skip((page - 1) * limit).limit(limit);
+      const token = req.headers.authorization?.split(" ")[1];
+  
+      if (!token) {
+        // return products if user is not logged in
+        return res.status(200).send(products);
+      }
+  
+      const decoded = jwt.verify(token, "my_signature");
+      const cart = await CartModel.findOne({ "userID": decoded.userID });
+  
+      if (!cart || cart.items.length === 0) {
+        // return products if user is logged in but has no items in cart
+        return res.status(200).send(products);
+      }
+  
+      // update products with added quantity if user is logged in and has items in cart
+      const updatedData = products.map((prod) => {
+        const cartItem = cart.items.find((item) => String(item.productID) === String(prod._id));
+        if (cartItem) {
+          prod.addedQuantity = cartItem.quantity;
+        }
+        return prod;
+      });
+  
+      return res.status(200).send(updatedData);
+  
+    } catch (error) {
+      return res.status(400).send({ "msg": error.message });
+    }
 }
+  
 
 
 // Get single product data
@@ -106,20 +114,19 @@ const getSingleProduct = async (req, res) => {
     const token = req.headers.authorization?.split(" ")[1];
     const cart = token ? await CartModel.findOne({ "userID": jwt.verify(token, "my_signature").userID }) : null;
     try {
-      const product = await ProductModel.findOne({ _id: prodID });
-      const cartItem = cart?.items.find(elem => elem.prodID === prodID);
-      console.log(cartItem)
-      if (cartItem) {
-        product.addedQuantity = cartItem.quantity;
-      }
-      res.status(200).send(product);
+        const product = await ProductModel.findOne({ _id: prodID });
+        const cartItem = cart?.items.find(elem => elem.prodID === prodID);
+        console.log(cartItem)
+        if (cartItem) {
+            product.addedQuantity = cartItem.quantity;
+        }
+        res.status(200).send(product);
     } catch (error) {
-      res.status(400).send({ "msg": error.message });
+        res.status(400).send({ "msg": error.message });
     }
-  }
-  
+}
 
-// add/ remove from cart
+// update quantity in product and cart
 const updateProductData = async (req, res) => {
     const { _id, addedQuantity } = req.body;
     const token = req.headers.authorization.split(" ")[1];
@@ -128,12 +135,12 @@ const updateProductData = async (req, res) => {
     try {
         const product = await ProductModel.findOne({ _id });
         product.addedQuantity = addedQuantity;
-        const cartItem = cart.items.find(elem => elem.prodID === _id);
+        const cartItem = cart.items.find(elem => elem.productID == _id);
         if (cartItem) {
             cartItem.quantity = addedQuantity;
         }
-        else{
-            cart.items.push({prodID : _id, quantity : addedQuantity});
+        else {
+            cart.items.push({ productID: _id, quantity: addedQuantity });
         }
         cart.items = cart.items.filter((elem) => elem.quantity !== 0);
         await cart.save();
