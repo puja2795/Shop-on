e879dd88f115ad6d919f20953e2e2fd66f9e2a87
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-04-02 02:43:20 +0530

    added delete user, delete product and edit product functionality.

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,21 +1,61 @@
 const { UserModel } = require("../model/user.model");
-const {CartModel} = require("../model/cart.model");
+const { ProductModel } = require("../model/product.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Edit product details
-const editProduct = async (req,res) => {
+const editProduct = async (req, res) => {
+    // Admin token check
+    const { prodID } = req.params;
+    const payload = req.body;
+    try {
+        const product = await ProductModel.findByIdAndUpdate({ _id: prodID }, payload, {
+            new: true,
+        });
+        if (!product) {
+            return res.status(404).send({ msg: "Product not found" });
+        }
+        res.status(200).send(product);
+    } catch (error) {
+        res.status(400).send({ msg: error.message });
+    }
+};
 
-}
+
 
 // Delete a product
-const deleteProduct = async (req,res) => {
-
+const deleteProduct = async (req, res) => {
+    // Admin token check
+    const { prodID } = req.params;
+    try {
+        const deletedProduct = await ProductModel.findByIdAndDelete({ _id: prodID });
+        if (!deletedProduct) {
+            return res.status(404).send({ msg: "Product not found" });
+        }
+        const products = await ProductModel.find();
+        res.status(200).send(products.filter(product => product._id.toString() !== prodID));
+    } catch (error) {
+        res.status(400).send({ msg: error.message });
+    }
 }
+
 
 // Delete a user
-const deleteUser = async( req,res) => {
-
+const deleteUser = async (req, res) => {
+    // Admin token check
+    const { userID } = req.params;
+    try {
+        let deletedUser = await UserModel.findByIdAndDelete({ _id: userID });
+        if (!deletedUser) {
+            return res.status(404).send({ msg: "User not found" });
+        }
+        let userList = await UserModel.find();
+        res.status(200).send({ msg: "User deleted successfully", userList });
+    } catch (error) {
+        res.status(400).send({ "msg": error.message });
+    }
 }
 
-module.exports = {editProduct,deleteProduct,deleteUser }
+
+
+module.exports = { editProduct, deleteProduct, deleteUser }
