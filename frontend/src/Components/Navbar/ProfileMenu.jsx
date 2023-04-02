44ff0244836a20b96872dbfc3ef9af80e84703f5
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-04-03 00:55:29 +0530

    updated admin login page

--- frontend/src/Components/Navbar/ProfileMenu.jsx
+++ frontend/src/Components/Navbar/ProfileMenu.jsx
@@ -4,21 +4,35 @@ import { Link } from "react-router-dom";
 
 const ProfileMenu = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [token,setToken] = React.useState(null);
+  const [name, setName] = React.useState(null);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("name");
+  }
+ 
+
+ React.useEffect(()=>{
+  setToken( localStorage.getItem("token"));
+  setName(localStorage.getItem("name"));
+ },[])
+
 
   return (
     <Menu>
       <MenuButton variant="link" onMouseEnter={() => setIsOpen(true)}>
         <Text fontSize="17px" fontWeight={500}>
-          Profile
+          {token && name ? `${name.split(" ")[0]}` : "Profile"}
         </Text>
       </MenuButton>
       <MenuList onMouseLeave={() => setIsOpen(false)}>
         {isOpen && (
           <>
-            <MenuItem>
+            <MenuItem background = {name && token ? "rgb(244, 51, 151)" : null }>
               <Link to="/userLogin">
-                <Text fontSize="17px" fontWeight={500}>
-                  User Login
+                <Text fontSize="17px" fontWeight={500} onClick={handleLogout}  >
+                 {name && token ? "Logout" : "User Login"}
                 </Text>
               </Link>
             </MenuItem>
