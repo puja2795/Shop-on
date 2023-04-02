9c9592d4e1ffb10b6a0b1bcddf20a4294b33a825
Pujarini Sahoo <pujarinisahoo95@gmail.com>
2023-04-02 17:57:20 +0530

    single product page added

--- frontend/src/Redux/productReducer/action.js
+++ frontend/src/Redux/productReducer/action.js
@@ -4,6 +4,7 @@ import {
   GET_PRODUCT_FOR,
   SET_PRODUCT_FOR,
   GET_PRODUCT_SUCCESS,
+  SET_FILTER,
 } from "../actionType";
 import axios from "axios";
 
@@ -27,8 +28,11 @@ export const getProductSuccess = (payload) => {
   return { type: GET_PRODUCT_SUCCESS, payload };
 };
 
+export const setFilter = (payload) => {
+  return { type: SET_FILTER, payload };
+};
+
 export const getProducts = (params) => (dispatch) => {
-  console.log(params);
   dispatch(productRequest());
   axios
     .get("http://localhost:8080/product", { params })
--- frontend/src/Redux/productReducer/reducer.js
+++ frontend/src/Redux/productReducer/reducer.js
@@ -1,30 +1,42 @@
 import {
-  GET_PRODUCT_FOR,
   GET_PRODUCT_SUCCESS,
   PRODUCT_FAILURE,
   PRODUCT_REQUEST,
+  SET_FILTER,
   SET_PRODUCT_FOR,
 } from "../actionType";
 
 const initState = {
-  productFor: "",
+  productFor: localStorage.getItem("productFor") || "",
   isLoading: false,
   isError: false,
   products: [],
+  filters: {
+    men: {},
+    women: {},
+    kids: {},
+  },
 };
 
 export const reducer = (state = initState, { payload, type }) => {
   switch (type) {
     case SET_PRODUCT_FOR:
       return { ...state, productFor: payload };
-    case GET_PRODUCT_FOR:
-      return state;
     case PRODUCT_REQUEST:
       return { ...state, isLoading: true };
     case PRODUCT_FAILURE:
       return { ...state, isError: true };
     case GET_PRODUCT_SUCCESS:
-      return { ...state, products: payload };
+      return { ...state, products: payload, isLoading: false };
+    case SET_FILTER:
+      if (state.productFor != "") {
+        return {
+          ...state,
+          filters: { ...state.filters, [state.productFor]: payload },
+        };
+      }
+      return state;
+
     default:
       return state;
   }
