5141fc71132c1ac866e767edf17097f89d54aa5f
Pujarini Sahoo <pujarinisahoo95@gmail.com>
2023-04-02 22:25:45 +0530

    "final work"

--- frontend/src/Components/Navbar/SearchBar.jsx
+++ frontend/src/Components/Navbar/SearchBar.jsx
@@ -1,17 +1,29 @@
-import { Box, Input, InputGroup, InputLeftElement, Stack } from '@chakra-ui/react'
-import React from 'react'
-import { FiSearch } from 'react-icons/fi';
+import {
+  Box,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+} from "@chakra-ui/react";
+import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const SearchBar = () => {
-    return (
-        <InputGroup bg={"white"} w="420px" size="lg" >
-            <InputLeftElement
-                pointerEvents='none'
-                children={<FiSearch color='gray' />}
-            />
-            <Input type='text' placeholder='Try Saree, Kurti or Search by Product Code' borderRadius={"3px"} color={"grey"} border={"1px solid #EDE8E8"} />
-        </InputGroup>
-    );
+  return (
+    <InputGroup bg={"white"} w="420px" size="lg">
+      <InputLeftElement
+        pointerEvents="none"
+        children={<FiSearch color="gray" />}
+      />
+      <Input
+        type="text"
+        placeholder="Try Saree, Kurti or Search by Product Code"
+        borderRadius={"3px"}
+        color={"grey"}
+        border={"1px solid #EDE8E8"}
+      />
+    </InputGroup>
+  );
 };
 
-export default SearchBar
+export default SearchBar;
